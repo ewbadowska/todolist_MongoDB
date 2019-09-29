d779d4682260fa1869f7270c8c887582637b2e6d
Ewelina Badowska <ewbadowska@gmail.com>
2019-09-29 12:38:35 +0200

    connect to mongoDB Atlas

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://admin-ewelina:admin123@cluster0-tg7z6.mongodb.net/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const itemsSchema = {
     name: String
